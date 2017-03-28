5da3e5dac7eb6fa2e52f7caf76581e4a574f4205
Rogerio Chaves <rogeriocfj@gmail.com>
2017-03-27 22:17:07 -0300

    Listen to addToCard events from the window

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () =>
-  <sidebar>
-    <h2>Cart</h2>
-    <p>Your cart is empty</p>
-  </sidebar>;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('addToCart', (event) => {
+      this.setState({ products: [...this.state.products, event.detail] });
+    }, false);
+  }
+
+  render () {
+    return (
+      <sidebar>
+        <h2>Cart</h2>
+        { this.renderProducts() }
+      </sidebar>
+    );
+  }
+
+  renderProducts () {
+    if (this.state.products.length === 0) {
+      return <p>Your cart is empty</p>;
+    }
+    return <ul>{ this.state.products.map(this.renderProduct) }</ul>;
+  }
+
+  renderProduct (product, index) {
+    return <li key={index}>{ product.product } - ${ product.price }</li>;
+  }
+}
