b654b779d194d499fecb013dc37c1640547231d7
Rogerio Chaves <rogeriocfj@gmail.com>
2017-04-01 00:47:00 -0300

    Render with webcomponents

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('cart-root')
-);
+class Cart extends HTMLElement {
+  attachedCallback() {
+    ReactDOM.render(<App />, this.createShadowRoot());
+  }
+}
+document.registerElement('microfrontends-cart', Cart);
